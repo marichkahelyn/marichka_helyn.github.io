6487be56ce1941933b88d296fbcc1b69d6dd7d49
Марічка <marichka.helyn.oi.2024@lpnu.ua>
2026-03-30 23:17:03 +0300

    add dist folder

--- LR3/vite.config.js
+++ LR3/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/marichka_helyn.github.io/LR3-dist/',
-  build: {
-    outDir: '../LR3-dist',  // ← білд іде в окрему папку
-  }
+  base: '/marichka_helyn.github.io/LR3/',
 })
