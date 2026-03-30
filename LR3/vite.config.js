e54306d6420f1faf9adbc0ad89fc5812626204e9
Марічка <marichka.helyn.oi.2024@lpnu.ua>
2026-03-30 22:30:54 +0300

    deploy LR3

--- LR3/vite.config.js
+++ LR3/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Обов'язково зі слешами з обох боків
-});
+  base: '/LR3/',  // ← просто назва папки
+})
