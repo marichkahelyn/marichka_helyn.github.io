13c24878a0736ce96c24b03526246bbb850f454b
Марічка <marichka.helyn.oi.2024@lpnu.ua>
2026-03-30 23:05:56 +0300

    deploy LR3

--- LR3/vite.config.js
+++ LR3/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/marichka_helyn.github.io/LR3/',  // ← просто назва папки
+  base: '/marichka_helyn.github.io/LR3-dist/',
+  build: {
+    outDir: '../LR3-dist',  // ← білд іде в окрему папку
+  }
 })
